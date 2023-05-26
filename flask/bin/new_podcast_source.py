269b60753961e86925d26509de6360cf7f6a405b
armory8854 <garrettsmith92@pm.me>
2023-05-26 01:07:44 -0400

    Finalized grabbing the podcast funding link

--- flask/bin/new_podcast_source.py
+++ flask/bin/new_podcast_source.py
@@ -2,6 +2,9 @@ from .parser import urlPagination
 from .db import newPodcastSourceDB, gatherSettings
 
 def newPodcastSource(db_file, new_podcast_source):
+    # Change url pagination to make 
+    # max_downloads=1
+    # by default - this is not needed at all here
     settings_dict = gatherSettings(db_file)
     max_downloads = settings_dict['max_downloads']
     values = urlPagination(db_file, new_podcast_source, max_downloads)
