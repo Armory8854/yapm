4bd87b6f1985e2c5cca1132e037aac3b7c3e93e5
armory8854 <garrettsmith92@pm.me>
2023-06-27 00:12:07 -0400

    more safe wildcard formatting

--- podman.sh
+++ podman.sh
@@ -59,11 +59,9 @@ function delete-all() {
     podman rm "localhost/yapm:latest" 
     podman rm "docker.io/binwiederhier/ntfy:v2.5.0" 
     rm "$YAPM_DATA_PATH"/{database.db,subscriptions.opml} 
-    rm -r "$YAPM_PODCAST_PATH"/*
-    for image in $(ls "$YAPM_IMAGE_PATH");
-    do
-        rm "$YAPM_IMAGE_PATH"/"$image"
-    done
+    rm "$YAPM_PODCAST_PATH"/*/*.opus
+    rmdir "$YAPM_PODCASTPATH"/*
+    rm "$YAPM_IMAGE_PATH"/*.jpg
 }
 
 ## Dev Functions
